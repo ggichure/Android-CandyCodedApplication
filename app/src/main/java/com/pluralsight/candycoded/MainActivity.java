a2242d03cc85db9d841c0cb0b6ee3bf4ba1b575c
ggichure <g.gichure@outlook.com>
2019-10-30 12:49:40 +0300

    Completed module

--- app/src/main/java/com/pluralsight/candycoded/MainActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/MainActivity.java
@@ -82,7 +82,12 @@ public class MainActivity extends AppCompatActivity {
     // ***
     // TODO - Task 1 - Show Store Information Activity
     // ***
-
+ @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent InfoIntent = new Intent(this, InfoActivity.class);
+        startActivity(InfoIntent);
+        return super.onOptionsItemSelected(item);
+    }
     private void addCandiesToDatabase(Candy[] candies) {
         SQLiteDatabase db = candyDbHelper.getWritableDatabase();
 
